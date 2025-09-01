d431f5cc0dbfc47c43deb66b4c015e1a00333b77
adepegba1 <adepegbadavid@rocketmail.com>
2025-09-01 17:10:27 +0100

    Week01 Exercise2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,128 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade: ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+        string letterGrade = "";
+        string letterSymbol = "";
+
+        if (numberGrade >= 70)
+        {
+            if (numberGrade % 10 >= 7)
+            {
+                if (numberGrade >= 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (numberGrade >= 80)
+                {
+                    letterGrade = "B";
+                    letterSymbol = "+";
+                }
+                else
+                {
+                    letterGrade = "C";
+                    letterSymbol = "+";
+                }
+            }
+            else if (numberGrade % 10 < 3)
+            {
+                letterSymbol = "-";
+                if (numberGrade >= 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (numberGrade >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else
+                {
+                    letterGrade = "C";
+                }
+            }
+            else
+            {
+                if (numberGrade > 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (numberGrade > 80)
+                {
+                    letterGrade = "B";
+                }
+                else
+                {
+                    letterGrade = "C";
+                }
+            }
+            Console.WriteLine($"Your letter grade is {letterGrade}{letterSymbol}");
+            Console.WriteLine("Congratulation you pass the course");
+        }
+        else
+        {
+            if (numberGrade >= 60)
+            {
+                if (numberGrade % 10 >= 7)
+                {
+                    letterSymbol = "+";
+                    letterGrade = "D";
+                }
+                else if (numberGrade % 10 < 3)
+                {
+                    letterSymbol = "-";
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "D";
+                }
+                Console.WriteLine($"Your letter grade is {letterGrade}{letterSymbol}");
+            }
+            else
+            {
+                letterGrade = "F";
+                Console.WriteLine($"Your letter grade is {letterGrade}");
+            }
+            Console.WriteLine("You failed the course, try again next time");
+        }
+
+
+        // if (numberGrade >= 70)
+        // {
+        //     if (numberGrade >= 90)
+        //     {
+        //         letterGrade = "A";
+        //         Console.WriteLine(letterGrade);
+        //     }
+        //     else if (numberGrade >= 80)
+        //     {
+        //         letterGrade = "B";
+        //         Console.WriteLine(letterGrade);
+        //     }
+        //     else if (numberGrade >= 70)
+        //     {
+        //         letterGrade = "C";
+        //         Console.WriteLine(letterGrade);
+        //     }
+        //     Console.WriteLine("Congratulation you pass the course");
+        // }
+        // else
+        // {
+        //     if (numberGrade >= 60)
+        //     {
+        //         letterGrade = "D";
+        //         Console.WriteLine(letterGrade);
+        //     }
+        //     else
+        //     {
+        //         letterGrade = "F";
+        //         Console.WriteLine(letterGrade);
+        //     }
+        //     Console.WriteLine("You failed the course, try again next time");
+        // }
     }
+
+
 }
