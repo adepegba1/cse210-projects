88cfd5c0b83755648bd2ad8bbc07150506802d15
adepegba1 <adepegbadavid@rocketmail.com>
2025-09-01 22:57:11 +0100

    week 01 exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string replay;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int count = 1;
+
+            Console.Write("What your guess: ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+
+            while (guessNumber != magicNumber)
+            {
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+                Console.Write("What your guess: ");
+                guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                count++;
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"The number of time you play is {count}");
+            Console.WriteLine("Do you want to play again: ");
+            replay = Console.ReadLine();
+
+        } while (replay == "yes");
     }
 }
